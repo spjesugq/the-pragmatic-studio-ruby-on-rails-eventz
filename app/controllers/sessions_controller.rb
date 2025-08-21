02a6180ce572747bc7ca8e656ec9050e9678f646
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-21 11:56:30 -0600

    Add Authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to(user, notice: "Welcome back, #{user.name}!")
+    else
+      flash.now[:alert] = "Invalid email/password combination!"
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
