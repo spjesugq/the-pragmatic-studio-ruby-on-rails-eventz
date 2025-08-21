b3dd26f01bc12d060e93c7f3858ed18a1bf7a927
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-21 10:42:57 -0600

    Add User Account Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_20_161519) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_21_161001) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -31,6 +31,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_20_161519) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_registrations_on_event_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "registrations", "events"
