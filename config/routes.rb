c421b89f5139d33e9bc11b753b579e06cd53a3ff
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-23 20:33:02 -0600

    Add Through Associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  resources :likes
   root "events#index"
   
   resources :users
   get "signup" => "users#new"
-
+  
   resource :session, only: [:new, :create, :destroy]
   get "signin" => "sessions#new"
-
+  
   resources :events do
     resources :registrations
+    resources :likes
   end
 end
