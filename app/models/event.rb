69f9d647b42676a656d5136f50876ee649ac33ff
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-25 10:04:00 -0600

    Add Custom Scopes and Routes

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,9 +14,10 @@ class Event < ApplicationRecord
     message: "must be a JPG or PNG image"
   }
 
-  def self.upcoming
-    where("starts_at > ?", Time.now).order("starts_at")
-  end
+  scope :upcoming, -> { where("starts_at > ?", Time.now).order("starts_at") }
+  scope :past, -> { where("starts_at < ?", Time.now).order("starts_at") }
+  scope :free, -> { upcoming.where(price: 0.0).order(:name) }
+  scope :recent, -> (max=3) { past.limit(max) }
 
   def free?
     price.blank? || price.zero?
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
     resources :registrations
     resources :likes
   end
+  get "events/filter/:filter" => "events#index", as: :filtered_events
 end
